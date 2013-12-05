40562c5e834d9b9771eeeb44f0dc09b37fba7bed
achingbrain <alex@achingbrain.net>
2013-12-05 18:20:21 +0000

    preparation for integration tests

---
+++ server/app.js
@@ -0,0 +1,107 @@
+var winston = require("winston"),
+	config = require("nconf"),
+	Container = require("wantsit").Container
+	Express = require("express"),
+	http = require("http"),
+	path = require("path"),
+	WebSocketServer = require("ws").Server,
+	EventEmitter = require("wildemitter"),
+	util = require("util");
+
+PM2Web = function(options) {
+	EventEmitter.call(this);
+
+	// set up arguments
+	config.overrides(options).argv().env().file(__dirname + "/../config.json");
+
+	this._container = new Container();
+	this._container.register("config", config);
+
+	// set up logging
+	this._container.createAndRegister("logger", winston.Logger, {
+		transports: [
+			new (winston.transports.Console)({
+				timestamp: true,
+				colorize: true
+			})
+		]
+	});
+
+	// web controllers
+	this._container.createAndRegister("homeController", require(__dirname + "/routes/Home"));
+
+	// listens for events
+	this._container.register("pm2InterfaceFactory", require("pm2-interface"));
+	this._container.createAndRegister("pm2Listener", require(__dirname + "/components/PM2Listener"));
+	this._container.createAndRegister("pm2ArgumentParser", require(__dirname + "/components/PM2ArgumentParser"));
+
+	// client interactions
+	this._container.createAndRegister("webSocketResponder", require(__dirname + "/components/WebSocketResponder"));
+	this._container.createAndRegister("webSocketServer", WebSocketServer, {
+		port: config.get("ws:port")
+	});
+
+	this._express = Express();
+
+	// all environments
+	this._express.set("port", config.get("www:port"));
+	this._express.set("view engine", "jade");
+	this._express.set("views", __dirname + "/views");
+	this._express.use(Express.logger("dev"));
+	this._express.use(Express.urlencoded())
+	this._express.use(Express.json())
+	this._express.use(Express.methodOverride());
+	this._express.use(this._express.router);
+	this._express.use(Express.static(__dirname + "/public"));
+
+	// development only
+	this._express.use(Express.errorHandler());
+
+	this._route("homeController", "/", "get");
+	this._route("homeController", "/hosts/:host", "get");
+};
+util.inherits(PM2Web, EventEmitter);
+
+PM2Web.prototype._route = function(controller, url, method) {
+	var component = this._container.find(controller);
+
+	this._express[method](url, component[method].bind(component));
+};
+
+PM2Web.prototype.setAddress = function(address) {
+	this._address = address;
+};
+
+PM2Web.prototype.getAddress = function(address) {
+	return this._address;
+};
+
+PM2Web.prototype.start = function() {
+	this._server = http.createServer(this._express);
+	this._server.listen(this._express.get("port"), function() {
+		this._container.find("logger").info("Express server listening on port " + this._server.address().port);
+
+		this.setAddress("http://127.0.0.1:" + this._server.address().port);
+		this.emit("start");
+	}.bind(this));
+
+	if(config.get("mdns:name")) {
+		try {
+			var mdns = require("mdns2");
+
+			// publish via Bonjour
+			var advert = mdns.createAdvertisement(mdns.tcp("http"), this._express.get("port"), {
+				name: config.get("mdns:name")
+			});
+			advert.start();
+		} catch(e) {
+			this._container.find("logger").warn("Could not start mdns argument - did mdns2 install correctly?", e.message);
+		}
+	}
+};
+
+PM2Web.prototype.stop = function() {
+	this._server.close();
+};
+
+module.exports = PM2Web;
--- test/integration/hosts.js
+++ test/integration/hosts.js
@@ -1,24 +1,48 @@
-var angular = require("angular"),
+var phantom = require("node-phantom"),
+	angular = require("angular"),
 	sinon = require("sinon"),
-	should = require("should");
+	should = require("should"),
+	PM2Web = require(__dirname + "/../../server/app");
 
-module.exports = {
-	setUp: function(done) {
-		this._app = angular.module("pm2-web");
+var pm2web;
+var browser;
 
-		angular.inject(function($controller) {
-			this._scope = {};
-			this._connectionController = $controller("ConnectionController", {$scope:scope});
-			this._systemController = $controller("SystemController", {$scope:scope});
-			this._processListController = $controller("ProcessListController", {$scope:scope});
-			this._hostListController = $controller("HostListController", {$scope:scope});
-		}.bind(this));
+var withBrowser = function(callback) {
+	if(!pm2web) {
+		pm2web = new PM2Web({www: {port: 0}});
+		pm2web.on("start", function() {
+			phantom.create(function(error, instance) {
+				if(error) {
+					throw new Error("Could not create phantomjs instance " + error);
+				}
 
-		done();
-	},
+				browser = instance;
 
-	"Something or other": function(test) {
-
-		test.done();
+				callback(browser);
+			});
+		});
+		pm2web.start();
+	} else {
+		callback(browser);
 	}
 };
+
+module.exports = {
+
+	"Should load the page": function(test) {
+		withBrowser(function(browser) {
+			browser.createPage(function(error, page) {
+				page.open(pm2web.getAddress(), function(error, status) {
+					status.should.equal("success");
+
+					test.done();
+				});
+			});
+		});
+	}
+};
+
+require("nodeunit").on("done", function() {
+	pm2web.stop();
+	process.exit();
+});
