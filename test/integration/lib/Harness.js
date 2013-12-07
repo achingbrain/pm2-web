5dd5678b40a80781c36448a3b45eabd875fd400c
achingbrain <alex@achingbrain.net>
2013-12-07 11:28:21 +0000

    integration test seems to work without selenium server running in the background so remove extra dependencies as they are very big

--- test/integration/lib/Harness.js
+++ test/integration/lib/Harness.js
@@ -1,38 +1,28 @@
 var PM2Web = require(__dirname + "/../../../server/app"),
 	Protractor = require("protractor"),
-	//remote = require("selenium-webdriver/remote"),
-	//webdrvr = require("webdrvr"),
 	EventEmitter = require("events").EventEmitter,
 	util = require("util"),
 	nodeunit = require("nodeunit");
 
-// starts selenium server, then pm2-web, then configures Protractor
-// to use our selenium server and emits a "ready" event.
+// starts pm2-web, then configures Protractor and emits a "ready" event.
 var Harness = function() {
 	EventEmitter.call(this);
 
 	var pm2web;
-	//var server = new remote.SeleniumServer(webdrvr.selenium.path, {
-	//	args: webdrvr.args
-	//});
-	//server.start().then(function(url) {
-	//	console.log('Selenium standalone server started at ' + url);
-		pm2web = new PM2Web({www: {port: 0}});
-		pm2web.on("start", function() {
-			var driver = new Protractor.Builder().
-				withCapabilities(Protractor.Capabilities.phantomjs()).
-				build();
+	pm2web = new PM2Web({www: {port: 0}});
+	pm2web.on("start", function() {
+		var driver = new Protractor.Builder().
+			withCapabilities(Protractor.Capabilities.phantomjs()).
+			build();
 
-			this.emit("ready", Protractor.wrapDriver(driver), pm2web);
-		}.bind(this));
-		pm2web.start();
-	//}.bind(this));
+		this.emit("ready", Protractor.wrapDriver(driver), pm2web);
+	}.bind(this));
+	pm2web.start();
 
 	nodeunit.on("done", function() {
 		console.info("All tests done, shutting down");
 
 		pm2web.stop();
-		//server.stop();
 
 		setTimeout(function() {
 			// something in pm2-interface does not tear it's connections
