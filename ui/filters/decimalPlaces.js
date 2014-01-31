40554eb3838491be7909273a9f7a4b327ea274b7
achingbrain <alex@achingbrain.net>
2014-01-31 18:23:39 +0000

    be a bit more defensive

--- ui/filters/decimalPlaces.js
+++ ui/filters/decimalPlaces.js
@@ -1,6 +1,10 @@
 
 module.exports = function() {
 	return function(number, decimalPlaces) {
+		if(!number) {
+			return 0;
+		}
+
 		return number.toFixed(decimalPlaces);
 	}
 };
--- ui/filters/memory.js
+++ ui/filters/memory.js
@@ -3,6 +3,10 @@ module.exports = function() {
 	var sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB"];
 
 	return function(bytes) {
+		if(!bytes) {
+			return "0B";
+		}
+
 		for(var i = sizes.length; i > 0; i--) {
 			var step = Math.pow(1024, i);
 
